8dc7ba19f4d0a24a32d3f91728bf0ae8b13964bc
Dev Arora <115332419+Devarora-0981@users.noreply.github.com>
2022-10-23 16:23:50 +0530

    Update dev_status_checker.py

--- dev_status_checker.py
+++ dev_status_checker.py
@@ -23,6 +23,7 @@ CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
 MESSAGE_ID = int(os.environ["MESSAGE_ID"])
 BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
 GRP_ID = os.environ.get("GRP_ID")
+GRP_USN = os.environ.get("GRP_USN")
 
 async def main_teletips():
     async with app:
@@ -42,11 +43,14 @@ async def main_teletips():
                             for bot_admin_id in BOT_ADMIN_IDS:
                                 try:
                                     await app.join_chat("Devbotz")
+                                    await app.join_chat({GRP_USN})
                                     await app.join_chat("We_rfriends")
                                     await app.send_message(int(GRP_ID), f"🚨 **Beep! Beep!! @{bot} is down** ❌")
                                 except Exception:
                                     pass
                             await app.read_chat_history(bot)
+                            await app.join_chat("Devbotz")
+                            await app.join_chat({GRP_USN})
                         else:
                             xxx_teletips += f"\n\n🤖  **@{bot}**\n └ **Online** ✅"
                             await app.read_chat_history(bot)
