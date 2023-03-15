9a9b887341197845de3bdf1e276070bddc193178
Dev Arora <115332419+Devarora-0981@users.noreply.github.com>
2023-03-15 20:30:37 +0530

    Update dev_status_checker.py

--- dev_status_checker.py
+++ dev_status_checker.py
@@ -18,10 +18,10 @@ app = Client(
 
 TIME_ZONE = os.environ["TIME_ZONE"]
 BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
-CHANNEL_ID = int(os.environ["CHANNEL_ID"])
+CHANNEL_ID = int(os.environ["CHANNEL_ID"])  #CHANNEL_ID is for the group/channel where the checker will update the status of bots.
 MESSAGE_ID = int(os.environ["MESSAGE_ID"])
 BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
-GRP_ID = os.environ.get("GRP_ID")
+GRP_ID = os.environ.get("GRP_ID")  #GRP_ID is for logs group where the checker will send warnings of offline bots.
 CHANNEL_NAME = os.environ.get("CHANNEL_NAME", "@DevBotz")
 
 async def main_devchecker():
